0440bb6a867b29cbbfe9a98f41acc50cd2fa1e63
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-04 20:45:59 -0700

    implement working search bar

--- client/searchService.js
+++ client/searchService.js
@@ -2,18 +2,17 @@
   'use strict';
 
   angular.module('productSearch')
-    .service('SearchService', function ($http) {
+  	.service('SearchService', function ($http) {
 
     var urlBase = 'https://api.gilt.com/v1/';
+
 
     this.getProducts = function(query) {
       return $http.get(urlBase +'products?q=' + query + '&apikey=' + GILT_API_KEY)
         .then(function(res) {
-          return res.products;
+          return res.data.products; 
         });
     }
-
-
   })
 
 })();
