999582b4851d667f6ea33cd8969699f4afa61fcb
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-05 17:47:34 -0700

    add helper modules

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,10 +1,13 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var path = require('path');
 
+var rootPath = path.join(__dirname, '../..');
 
 module.exports = function (app, express) {
   app.use(morgan('dev'));
-  app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../../client'));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(rootPath + '/client'));
+  app.use('/node_modules', express.static(rootPath + '/node_modules'));
 };
