16f8408fd9601d74e94c349c8c66c321d198366e
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-05 17:45:57 -0700

    update product format

--- client/product/productController.js
+++ client/product/productController.js
@@ -2,14 +2,16 @@
   'use strict';
 
   angular.module('app')
-    .controller('ProductController', function($routeParams, $scope, ProductService) {
+    .controller('ProductController', function($routeParams, $scope, ProductService, CartService) {
       ProductService.getProductById($routeParams.productId)
         .then(function(product) {
           $scope.product = product;
         })
         .catch(function(error) {
           console.log(error);
-        })
+        });
+
+      $scope.add = CartService.addToCart;
   });
 
 })();
