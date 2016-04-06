5e5bff7fa9b8953ddd4e74aad120425306d02891
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-05 17:47:34 -0700

    add helper modules

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,11 +1,23 @@
+var path = require('path');
 var cartController = require('../cart/cartController.js');
-
 var helpers = require('./helpers.js');
 
+var rootPath = path.join(__dirname, '../..');
+
 module.exports = function(app, express) {
+  
   app.get('/api/carts', cartController.allCartItems);
   app.post('/api/carts', cartController.addToCart);
 
+  
+  app.get('/', function(req, res) {
+    res.sendFile(rootPath + '/client/index.html');
+  });
+  
+  app.get('/*', function(req, res) {
+    res.sendFile(rootPath + '/client/index.html');
+  });
+  
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 }
